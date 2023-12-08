8cb16b058d4c375748da043d8c4e833bac6783bb
alberticona <albert.ticona@ucsp.edu.pe>
2023-12-08 04:12:31 -0500

    Presentation Albert Ticona

--- src/components/AlberTicona.jsx
+++ src/components/AlberTicona.jsx
@@ -1,4 +1,14 @@
 const AlberTicona = () => {
-  return <div>AlberTicona</div>;
+  return (
+    <article className="text-center p-4 text-lg">
+      <header className="w-[230px] shadow-md rounded-md overflow-hidden mx-auto">
+        <img src="/AlbertTicona.jpg" alt="" />
+      </header>
+      <h2 className="font-bold">Junior Pacheco</h2>
+      <p className="text-slate-500 font-semibold">
+        Instructor de React en Academlo
+      </p>
+    </article>
+  );
 };
 export default AlberTicona;
